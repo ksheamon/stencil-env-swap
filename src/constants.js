2222887ff36fa52c0c1c99ea971d945a5cf7ec00
Kimberly Sheamon <ksheamon@gmail.com>
2025-01-29 14:45:07 -0500

    bump stencil-cli & node; add package manager prompts

--- src/constants.js
+++ src/constants.js
@@ -10,6 +10,7 @@ const PATH_ENVKEYS = `${ROOT_DIR}/env/keys`;
 const BASE_CONFIG = `${ROOT_DIR}/config.json`;
 
 const ALLENVS = ['dev','stage','uat','prod'];
+const ALLPMS = ['npm','yarn','pnpm'];
 const STENCIL_HOST = 'https://api.bigcommerce.com';
 
 // globalize the pattern for BC store URLs
@@ -23,6 +24,13 @@ const ENVOPTS = ALLENVS.map((env) => {
     }
 });
 
+const PMOPTS = ALLPMS.map((pm) => {
+    return {
+        'name': pm,
+        'value': pm
+    }
+});
+
 export {
     ALLENVS,
     BASE_CONFIG,
@@ -32,6 +40,7 @@ export {
     PATH_ENV,
     PATH_ENVCONFIG,
     PATH_ENVKEYS,
+    PMOPTS,
     STENCIL_HOST,
     STOREURL_PATTERN
 }
